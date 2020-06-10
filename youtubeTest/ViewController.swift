656f3425219f4ecb757546c7affea1b59a623907
T1me <t1me@t1gate.com>
2020-06-10 17:57:38 +0800

    Init Youtube project

---
+++ youtubeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtubeTest
+//
+//  Created by T1me on 10/6/2020.
+//  Copyright © 2020 T1me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
